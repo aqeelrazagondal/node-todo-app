2fcaa7e2f35686a19d5bf2cd6bfab44459edbd50
aqeelraz146@gmail.com <aqeelrazagondal@gmail.com>
2018-02-22 01:16:16 +0500

    Update file mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-//mongoose.connect('mongodb://aqeel:raza@ds245218.mlab.com:45218/node-todo-app');
+//mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://aqeel:raza@ds245218.mlab.com:45218/node-todo-app');
+mongoose.connect('mongodb://aqeel:raza@ds245218.mlab.com:45218/node-todo-app');
 
 module.exports = {mongoose};
